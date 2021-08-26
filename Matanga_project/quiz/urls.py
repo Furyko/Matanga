688094163390fa24d64e18501fc6f885966f9905
samuguayfenix@gmail.com <samuguayfenix@gmail.com>
2021-08-25 21:33:36 -0300

    Movidos los estilos a quiz/static/estilos.css

--- Matanga_project/quiz/urls.py
+++ Matanga_project/quiz/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin', views.admin, name='admin'),
-    path('fin', views.fin, name='fin'),
-    path('juego', views.juego, name='juego'),
-    path('mapa', views.mapa, name='mapa'),
-    path('ranking', views.ranking, name='ranking'),
-    path('recuperar', views.recuperar, name='recuperar'),
-    path('registro', views.registro, name='registro'),
-    path('victoria', views.victoria, name='victoria'),
+    path('admin/', views.admin, name='admin'),
+    path('fin/', views.fin, name='fin'),
+    path('juego/', views.juego, name='juego'),
+    path('mapa/', views.mapa, name='mapa'),
+    path('ranking/', views.ranking, name='ranking'),
+    path('recuperar/', views.recuperar, name='recuperar'),
+    path('registro/', views.registro, name='registro'),
+    path('victoria/', views.victoria, name='victoria'),
     path('', views.inicio, name='inicio'),
 ]
