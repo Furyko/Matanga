b056f63b498f01dd4c49fab9e40a92d69edfee19
BajoPresion <luigi.diaz@elechaco.edu.ar>
2021-08-27 20:16:48 -0300

    prueba

--- Matanga_project/quiz/admin.py
+++ Matanga_project/quiz/admin.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
+admin.site.register(Usuario)
+admin.site.register(Date)
 admin.site.register(Partida)
 admin.site.register(Categoria)
-class RespuestasEnLinea(admin.TabularInline):
-    modelo = Respuesta
+admin.site.register(Quiz)
+admin.site.register(Dificultad)
 
-class PreguntaAdmin(admin.ModelAdmin):
-    enlineas = [RespuestasEnLinea]
 
-admin.site.register(Pregunta, PreguntaAdmin)
-admin.site.register(Respuesta)
-admin.site.register(Usuario)
+
+
+
 
--- Matanga_project/quiz/models.py
+++ Matanga_project/quiz/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import IntegerField
+from django.db.models.fields import DateField, DateTimeField, IntegerField
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -11,62 +11,48 @@ DIFICULTAD_RESPUESTAS = (
 )
 
 
-
-
 class Usuario(models.Model):
     nombre= models.CharField(max_length=50)
     email = models.CharField(max_length=80)
     clave= models.CharField(max_length=100)
     admin = models.BooleanField(default=False)
-    id_partida = models.ForeignKey('partida', models.DO_NOTHING, db_column='id_partida', blank=True, null=True)
 
 
+class Date(models.Model):
+    date = DateTimeField (auto_now=True)
 
-class Partida (models.Model):
-    puntaje_maximo= models.IntegerField(null=True)
-    puntaje_juego= models.IntegerField(null=True)
-    dificultad = models.CharField(max_length=20, default="normal")
+
+class Partida(models.Model):
+    puntaje_maximo= models.IntegerField(blank=True, null=True)
+    puntaje_juego= models.IntegerField(blank=True, null=True)
+    victoria= models.BooleanField(default=False)
+
+    id_date = models.ForeignKey('date', models.DO_NOTHING, db_column='id_date', blank=True, null=True) #Usar now
+    
+    id_usuario = models.ForeignKey('usuario', models.DO_NOTHING, db_column='id_usuario', blank=True, null=True)
+    
     id_categoria = models.ForeignKey('categoria', models.DO_NOTHING, db_column='id_categoria', blank=True, null=True)
 
-
-
+    id_dificultad = models.ForeignKey('dificultad', models.DO_NOTHING, db_column='id_dificultad', blank=True, default=2) 
+    #ID 2 es normal en dificultad
+   
 
 class Categoria(models.Model):
-    Categoria = models.CharField(max_length=50)
-    topico = models.CharField(max_length=200)
-    id_pregunta = models.ForeignKey('pregunta', models.DO_NOTHING, db_column='id_pregunta', blank=True, null=True)
-    
-
-    def __str__(self):
-        return f"{self.nombre}-{self.topico}"
-
-    def obtener_preguntas(self):
-        return self.definir_pregunta.all()
-
-    class Meta:
-        verbose_name_plural = 'Quizes'
+    categoria= models.CharField(max_length=100, blank=True, null=True)
+    id_quiz = models.ForeignKey('quiz', models.DO_NOTHING, db_column='id_quiz', blank=True, null=True)
 
 
+class Quiz(models.Model):
+    #categoria_preg = models.CharField(max_length=100, blank=True, null=True)
+    pregunta = models.CharField(max_length=200, blank=True, null=True)
+    respuesta_1 = models.CharField(max_length=200, blank=True, null=True)
+    respuesta_2 = models.CharField(max_length=200, blank=True, null=True)
+    respuesta_3 = models.CharField(max_length=200, blank=True, null=True)
+    respuesta_4 = models.CharField(max_length=200, blank=True, null=True)
+    respuesta_5 = models.CharField(max_length=200, blank=True, null=True)
+    correcta = models.IntegerField(default=1, blank=True, null=True)
 
-
-class Pregunta(models.Model):
-    texto_pregunta = models.CharField(max_length=200)
-    id_respuesta = models.ForeignKey('respuesta', models.DO_NOTHING, db_column='id_respuesta', blank=True, null=True)
-
-    def obtener_respuestas(self):
-        return self.definir_respuesta.all()
-
-    def __str__(self):
-        return str(self.texto_pregunta)
-
-
-
-
-class Respuesta(models.Model):
-    texto_respuesta = models.CharField(max_length=200)
-    opcion_correcta = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"pregunta: {self.pregunta.texto_pregunta}, respuesta: {self.texto_respuesta}, correcta: {self.opcion_correcta}"
-
+class Dificultad(models.Model):
+    vida = IntegerField()
+    tiempo= IntegerField()
 
